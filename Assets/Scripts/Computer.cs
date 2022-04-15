1b8141bd1435ae0348c5b4ebd795e2b5f76d5bf7
suy00065 <suy00065@gmail.com>
2022-04-16 02:38:09 +0900

    menu

---
+++ Assets/Scripts/Computer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Computer : MonoBehaviour
+{
+    public GameObject MenuSet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))       // 마우스 클릭시
+        {
+            // 마우스로 클릭한 좌표값을 가져옴
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            // 해당 좌표에 있는 오브젝트를 찾음
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
+
+            //태그가 "computer"인 오브젝트 클릭시
+            if (hit.transform.gameObject.tag == "computer")
+            {
+                // null 값이 아니라면
+                if (hit.collider != null)
+                {
+                    // 메뉴 창을 끄거나 킴
+                    //if (MenuSet.activeSelf)
+                    //    MenuSet.SetActive(false);
+                    //else
+                    //    MenuSet.SetActive(true);
+                    MenuSet.SetActive(true);
+
+                }
+            }
+        }
+    }
+}
